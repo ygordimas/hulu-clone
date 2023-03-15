ab84a60ffb6f31e4414ce25e86316480333f7e02
Ygor Dimas <ygordimas.dev@gmail.com>
2023-03-15 10:14:34 +0000

    Results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
 import Nav from "@/components/Nav";
+import Results from "@/components/Results";
+import requests from "@/utils/requests";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <>
       <Head>
@@ -18,6 +20,23 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results results={results} />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3/${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
