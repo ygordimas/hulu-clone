41ec2308726d4b32d8e669bc9b264e344bd9f48b
Ygor Dimas <ygordimas.dev@gmail.com>
2023-03-15 08:43:25 +0000

    Navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,24 @@
-import { HomeIcon } from "@heroicons/react/24/outline";
+import {
+  HomeIcon,
+  BoltIcon,
+  CheckBadgeIcon,
+  RectangleStackIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <header className="">
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={BoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={CheckBadgeIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={RectangleStackIcon} />
+        <HeaderItem title="SEARCH" Icon={MagnifyingGlassIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
         className="object-contain"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
+import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Nav />
     </>
   );
 }
